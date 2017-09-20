bd8d67759bed0a7a86e2ecf6a81b4e35ca969311
RyanH98 <harer6@students.rowan.edu>
2017-09-20 13:27:05 -0400

    Multiple blink code and fixed some simple blink code

--- Simple Blink/MSP430G2553/blink.c
+++ Simple Blink/MSP430G2553/blink.c
@@ -7,15 +7,13 @@
 void main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;       // stop watchdog timer
-    P1DIR |= 0x41;                  // configure P1.0 and P1.7 as output
+    P1DIR |= 0x01;                  // configure P1.0 as output
 
     volatile unsigned int i;        // volatile to prevent optimization
 
     while(1)
     {
-        P1OUT ^= 0x40;              // toggle P1.0
-        for(i=5000; i>0; i--);     // delay
-        P1OUT ^= 0x01;              // toggle P1.7
+        P1OUT ^= 0x01;              // toggle P1.0
         for(i=5000; i>0; i--);      //delay
     }
 }
