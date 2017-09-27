0ee5939a6b292c4c167d93e66f87a017b32c4eac
RyanH98 <harer6@students.rowan.edu>
2017-09-27 13:06:50 -0400

    Finalized all pin assignments. All code working

--- Button Blink/MSP430FR6989/main.c
+++ Button Blink/MSP430FR6989/main.c
@@ -1,6 +1,8 @@
 #include <msp430.h>
 
-#define BUTTON BIT3                 //Define "BUTTON" as bit 3.
+//These pin assignments are for the FR6989.
+
+#define BUTTON BIT1                 //Define "BUTTON" as bit 3.
 #define LED0 BIT0                   //Define "LED0" as bit 0.
 #define INP (P1IN & BUTTON)         //Define "INP" for checking if there is an input on pin 1.3.
 int i = 0;                          //Initialize int i for later use.
@@ -9,14 +11,13 @@ void main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;       //Stop watchdog timer
 
-    P1SEL &= (~LED0 & ~BUTTON);     //Select the I/O mode for pins 1.0 and 1.3.
-
     P1DIR |= LED0;                  //Set pin 1.0 (LED) as an output.
     P1OUT &= ~LED0;                 //Set the initial LED condition to off.
 
     P1DIR &= ~BUTTON;               //Set pin 1.3 (Button) as an input.
     P1REN |= BUTTON;                //Enable the pull resistor on pin 1.3.
     P1OUT |= BUTTON;                //Tell the pull resistor to pull up.
+    PM5CTL0 &= ~LOCKLPM5;            //Disable high impedance mode.
 
     while(1) {
         if(!INP && !i){             //The first time the button is pressed, toggle the LED once and then set i = 1.
