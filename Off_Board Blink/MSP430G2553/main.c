7dd056ea532d0bc4119665f572fe6aa6634d33f4
RyanH98 <harer6@students.rowan.edu>
2017-10-04 17:01:45 -0400

    more code for extra work (game)

--- Off_Board Blink/MSP430G2553/main.c
+++ Off_Board Blink/MSP430G2553/main.c
@@ -9,11 +9,21 @@ void main(void)
     WDTCTL = WDTPW | WDTHOLD;       // stop watchdog timer
     P1DIR |= LED1;                  //Configure pin 1.0 as an output.
 
-    int i = 0;        //Initialize int i for the for loop.
+    P1OUT &= LED1;                 //Set the initial LED condition to off.
 
-    while(1)
-    {
-        P1OUT ^= LED1;              //Toggle pin 1.0 (the LED).
-        for(i=5000; i>0; i--);      //Use a for loop to delay.
-    }
+    CCTL0 = CCIE;                   //Enable interrupts for the first capture/compare register.
+    TACTL = TASSEL_2 + MC_2 + ID_3; //Set the Clock_A control to:
+                                    //1. TASSEL_2 which selects SMCLK, the internal 1MHz clock.
+                                    //2. MC_2 which selects the continuous counting mode.
+                                    //3. ID_3 which sets an internal 8x clock divider.
+
+    __enable_interrupt();           //Enable interrupts.
+
+    __bis_SR_register(LPM0 + GIE);  //Enter low power mode with interrupts.
+
+}
+
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer_A (void) {
+    P1OUT ^= LED1;                  //Toggle the LED whenever the timer interrupts (about every 0.5 seconds).
 }
